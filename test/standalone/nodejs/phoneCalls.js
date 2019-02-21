4451dfd125d1a9e4af3a6d2813b08703a0fd7321
Soroush Saffari <sor.saffari@gmail.com>
2019-02-21 14:36:54 +0000

    wip

--- test/standalone/nodejs/phoneCalls.js
+++ test/standalone/nodejs/phoneCalls.js
@@ -12,35 +12,43 @@ fs.copySync(
     });
 
 const Grakn = require('../../../../external/graknlabs_client_nodejs_nosymlinks/');
+const papa = require("papaparse");
 
-const PhoneCallsCSVMigration = require("./PhoneCallsCSVMigration.js");
-const PhoneCallsJSONMigration = require("./PhoneCallsJSONMigration.js");
-const PhoneCallsXMLMigration = require("./PhoneCallsXMLMigration.js");
+console.log(fs.readdirSync('./'));
+
+const PhoneCallsCSVMigration = require("../generated/phoneCallsCSVMigration.js");
+//const PhoneCallsJSONMigration = require("../generated/phoneCallsJSONMigration.js");
+//const PhoneCallsXMLMigration = require("../generated/phoneCallsXMLMigration.js");
 
 beforeAll(async () => {
     await loadSchemaPhoneCalls();
 });
 
 loadSchemaPhoneCalls = async () => {
-    const defineQuery = fs.readFileSync("files/phone-calls/schema.gql");
+//    console.log(__filename);
+//    console.log(__dirname);
+    const defineQuery = fs.readFileSync("../../../files/phone-calls/schema.gql");
     const client = new Grakn("localhost:48555");
     const session = client.session("phone_calls");
-    const transaction = await session.transaction(grakn.TxType.WRITE);
+    const transaction = await session.transaction(Grakn.txType.WRITE);
     await transaction.query(defineQuery);
     transaction.commit();
     session.close();
 };
 
 describe("Test migration scripts for phone_calls", () => {
+//    test("CSV migration", async () => {
+//       console.log("yup");
+//    });
     test("CSV migration", async () => {
         await PhoneCallsCSVMigration.buildPhoneCallGraph();
     });
-
-    test("JSON migration", async () => {
-        await PhoneCallsJSONMigration.buildPhoneCallGraph();
-    });
-
-    test("XML migration", async () => {
-        await PhoneCallsXMLMigration.buildPhoneCallGraph();
-    });
+//
+//    test("JSON migration", async () => {
+//        await PhoneCallsJSONMigration.buildPhoneCallGraph();
+//    });
+//
+//    test("XML migration", async () => {
+//        await PhoneCallsXMLMigration.buildPhoneCallGraph();
+//    });
 });
