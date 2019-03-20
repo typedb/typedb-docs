b624bfbc2a5b74a2332b0faefe9ac06dcde07d76
sorsaffari <soroush@grakn.ai>
2019-03-20 13:33:40 +0000

    add view files

---
+++ views/colored-labels.js
@@ -0,0 +1,12 @@
+/**
+ * Within markdowns, we can place a Label Title in square brackets (eg: `[Label Title]`) to have it presented as an inline colored label.
+ *
+ * For this to work, the title used within the square brackets, as well as the the labels' background color and the url for it to point to, need to be present as a key in the `coloredLabels`.
+ */
+
+coloredLabels = {
+    "KGMS ONLY": {
+        color: "#efb507",
+        url: "https://grakn.ai/grakn-kgms"
+    }
+}
