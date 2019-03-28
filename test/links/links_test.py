19b72808a75d404795b643e826284add487c9d11
sorsaffari <soroush@grakn.ai>
2019-03-28 12:22:57 +0000

    include comments in links_test.py

--- test/links/links_test.py
+++ test/links/links_test.py
@@ -8,7 +8,7 @@ from nested_lookup import nested_lookup
 
 pattern_to_find_links = '(\[[^\]]*?\]\(\.\.\/)((\d+-[^\/\)]*(\/|))+)\)'
 pattern_to_find_anchors = '#+\s(.*)'
-pattern_to_find_template_calls = '{%\sinclude\s.*?\s%}'
+pattern_to_find_template_calls = '\{%\sinclude\s.*?\s%\}'
 
 pages = {}
 
@@ -40,10 +40,14 @@ for markdown_path in glob.iglob('./**/*.md'):
                                          .replace("site.data.", "")\
                                          .replace(".", "/")\
                                          .replace("_", "-") + ".yml"
+                    # although the directory name is expected to have - instead of _
+                    # file name is expected to have _ , so:
                     yaml_file_name = os.path.basename(yaml_file_path)
                     new_yaml_file_name = yaml_file_name.replace("-", "_")
                     yaml_file_path = yaml_file_path.replace(yaml_file_name, new_yaml_file_name)
+
                     yaml_content = yaml.load(open(yaml_file_path), Loader=yaml.CLoader)
+                    # looks for the value of all keys named `title`, convert them to an id and add them as anchors
                     for found_title in nested_lookup("title", yaml_content):
                         anchor = found_title.replace(r" ", "-").replace(r"/", "-").lower()
                         pages[title]["anchors"].append(anchor)
@@ -52,13 +56,11 @@ for markdown_path in glob.iglob('./**/*.md'):
 class LinksTest(unittest.TestCase):
 
         def test_native_links(self):
-
-            # assess the correctness of links
             errors = []
             for title, links_and_anchors in pages.items():
                 for link in links_and_anchors["links"]:
                     link_page = link.split("#")[0]
-                    link_page = link_page.split("?")[0]  # remove params
+                    link_page = link_page.split("?")[0]  # removes params
                     if link_page not in pages:
                         errors.append("The link [" + link_page + "] found in [" + title + "] is broken.")
 
@@ -73,6 +75,8 @@ class LinksTest(unittest.TestCase):
         def test_autolinks(self):
             errors = []
             autolink_keywords_path = "./views/autolink-keywords.js"
+            # the following mappings are required because links included in autolink-keywords.js are post-processed,
+            # whereas we're comparing them against raw links found in markdown files
             url_mapping = {
                 "../schema/concepts": "09-schema/01-concepts.md",
                 "../schema/rules": "09-schema/03-rules.md",
@@ -83,7 +87,6 @@ class LinksTest(unittest.TestCase):
                 "../query/aggregate-query": "10-query/06-aggregate-query.md",
                 "../query/compute-query": "10-query/07-compute-query.md"
             }
-
             client_page_mapping = {
                 "java": "01-java.md",
                 "python": "02-python.md",
@@ -92,8 +95,8 @@ class LinksTest(unittest.TestCase):
 
             with open(autolink_keywords_path) as autolink_keywords_file:
                 content = autolink_keywords_file.read().split("codeKeywordsToLink = ")[1]
-                content = re.sub(r'\/\/\s.*', '', content)
-                autolink_keywords = ast.literal_eval(content)
+                content = re.sub(r'\/\/\s.*', '', content)  # removes comments
+                autolink_keywords = ast.literal_eval(content)  # converts string to dict
                 common_base_url = "03-client-api/{client}"
 
                 for keyword in autolink_keywords["keywords"]:
