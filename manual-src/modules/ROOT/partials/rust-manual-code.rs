a35128a95d57e68ea5274d46d2f0a1ca12fad97d
Vladimir Izmalkov <vladimir@vaticle.com>
2024-02-12 10:38:28 +0000

    Update indentation and replace tx with transaction variable

--- manual-src/modules/ROOT/partials/rust-manual-code.rs
+++ manual-src/modules/ROOT/partials/rust-manual-code.rs
@@ -42,7 +42,7 @@ fn main() -> Result<(), Error> {
         {
             let session = Session::new(db, SessionType::Schema)?;
             {
-                let tx = session.transaction(TransactionType::Write)?;
+                let transaction = session.transaction(TransactionType::Write)?;
                 let define_query = "
                                     define
                                     email sub attribute, value string;
@@ -54,8 +54,8 @@ fn main() -> Result<(), Error> {
                                         plays friendship:friend;
                                     admin sub user;
                                     ";
-                tx.query().define(define_query).resolve()?;
-                tx.commit().resolve()?;
+                transaction.query().define(define_query).resolve()?;
+                transaction.commit().resolve()?;
             }
         }
         // end::define[]
@@ -63,158 +63,194 @@ fn main() -> Result<(), Error> {
 
     {   // tag::undefine[]
         let db = databases.get(DB_NAME)?;
-        let session = Session::new(db, SessionType::Schema)?;
-        let tx = session.transaction(TransactionType::Write)?;
-        let undefine_query = "undefine admin sub user;";
-        tx.query().undefine(undefine_query).resolve()?;
-        tx.commit().resolve()?;
+        {
+            let session = Session::new(db, SessionType::Schema)?;
+            {
+                let transaction = session.transaction(TransactionType::Write)?;
+                let undefine_query = "undefine admin sub user;";
+                transaction.query().undefine(undefine_query).resolve()?;
+                transaction.commit().resolve()?;
+            }
+        }
         // end::undefine[]
     }
 
     {   // tag::insert[]
         let db = databases.get(DB_NAME)?;
-        let session = Session::new(db, SessionType::Data)?;
-        let tx = session.transaction(TransactionType::Write)?;
-        let insert_query = "
-                            insert
-                            $user1 isa user, has name 'Alice', has email 'alice@vaticle.com';
-                            $user2 isa user, has name 'Bob', has email 'bob@vaticle.com';
-                            $friendship (friend:$user1, friend: $user2) isa friendship;
-                            ";
-        let _ = tx.query().insert(insert_query)?;
-        tx.commit().resolve()?;
+        {
+            let session = Session::new(db, SessionType::Data)?;
+            {
+                let transaction = session.transaction(TransactionType::Write)?;
+                let insert_query = "
+                                    insert
+                                    $user1 isa user, has name 'Alice', has email 'alice@vaticle.com';
+                                    $user2 isa user, has name 'Bob', has email 'bob@vaticle.com';
+                                    $friendship (friend:$user1, friend: $user2) isa friendship;
+                                    ";
+                let _ = transaction.query().insert(insert_query)?;
+                transaction.commit().resolve()?;
+            }
+        }
         // end::insert[]
     }
 
     {   // tag::match-insert[]
         let db = databases.get(DB_NAME)?;
-        let session = Session::new(db, SessionType::Data)?;
-        let tx = session.transaction(TransactionType::Write)?;
-        let match_insert_query = "
-                                match
-                                $u isa user, has name 'Bob';
-                                insert
-                                $new-u isa user, has name 'Charlie', has email 'charlie@vaticle.com';
-                                $f($u,$new-u) isa friendship;
-                                ";
-        let response_count = tx.query().insert(match_insert_query)?.count();
-        if response_count == 1 {
-            tx.commit().resolve()?;
-        } else {
-            tx.force_close();
+        {
+            let session = Session::new(db, SessionType::Data)?;
+            {
+                let transaction = session.transaction(TransactionType::Write)?;
+                let match_insert_query = "
+                                        match
+                                        $u isa user, has name 'Bob';
+                                        insert
+                                        $new-u isa user, has name 'Charlie', has email 'charlie@vaticle.com';
+                                        $f($u,$new-u) isa friendship;
+                                        ";
+                let response_count = transaction.query().insert(match_insert_query)?.count();
+                if response_count == 1 {
+                    transaction.commit().resolve()?;
+                } else {
+                    transaction.force_close();
+                }
+            }
         }
         // end::match-insert[]
     }
 
     {   // tag::delete[]
         let db = databases.get(DB_NAME)?;
-        let session = Session::new(db, SessionType::Data)?;
-        let tx = session.transaction(TransactionType::Write)?;
-        let delete_query = "
-                            match
-                            $u isa user, has name 'Charlie';
-                            $f ($u) isa friendship;
-                            delete
-                            $f isa friendship;
-                            ";
-        let _ = tx.query().delete(delete_query).resolve();
-        tx.commit().resolve()?;
+        {
+            let session = Session::new(db, SessionType::Data)?;
+            {
+                let transaction = session.transaction(TransactionType::Write)?;
+                let delete_query = "
+                                    match
+                                    $u isa user, has name 'Charlie';
+                                    $f ($u) isa friendship;
+                                    delete
+                                    $f isa friendship;
+                                    ";
+                let _ = transaction.query().delete(delete_query).resolve();
+                transaction.commit().resolve()?;
+            }
+        }
         // end::delete[]
     }
 
     {   // tag::update[]
         let db = databases.get(DB_NAME)?;
-        let session = Session::new(db, SessionType::Data)?;
-        let tx = session.transaction(TransactionType::Write)?;
-        let update_query = "
-                            match
-                            $u isa user, has name 'Charlie', has email $e;
-                            delete
-                            $u has $e;
-                            insert
-                            $u has email 'charles@vaticle.com';
-                            ";
-        let response_count = tx.query().update(update_query)?.count();
-        if response_count == 1 {
-            tx.commit().resolve()?;
-        } else {
-            tx.force_close();
+        {
+            let session = Session::new(db, SessionType::Data)?;
+            {
+                let transaction = session.transaction(TransactionType::Write)?;
+                let update_query = "
+                                    match
+                                    $u isa user, has name 'Charlie', has email $e;
+                                    delete
+                                    $u has $e;
+                                    insert
+                                    $u has email 'charles@vaticle.com';
+                                    ";
+                let response_count = transaction.query().update(update_query)?.count();
+                if response_count == 1 {
+                    transaction.commit().resolve()?;
+                } else {
+                    transaction.force_close();
+                }
+            }
         }
         // end::update[]
     }
 
     {   // tag::fetch[]
         let db = databases.get(DB_NAME)?;
-        let session = Session::new(db, SessionType::Data)?;
-        let tx = session.transaction(TransactionType::Read)?;
-        let fetch_query = "
-                            match
-                            $u isa user;
-                            fetch
-                            $u: name, email;
-                            ";
-        let response = tx.query().fetch(fetch_query)?;
-        for (i, json) in response.enumerate() {
-            println!("User #{}: {}", (i + 1).to_string(), json.unwrap().to_string())
+        {
+            let session = Session::new(db, SessionType::Data)?;
+            {
+                let transaction = session.transaction(TransactionType::Read)?;
+                let fetch_query = "
+                                    match
+                                    $u isa user;
+                                    fetch
+                                    $u: name, email;
+                                    ";
+                let response = transaction.query().fetch(fetch_query)?;
+                for (i, json) in response.enumerate() {
+                    println!("User #{}: {}", (i + 1).to_string(), json.unwrap().to_string())
+                }
+            }
         }
         // end::fetch[]
     }
 
     {   // tag::get[]
         let db = databases.get(DB_NAME)?;
-        let session = Session::new(db, SessionType::Data)?;
-        let tx = session.transaction(TransactionType::Read)?;
-        let get_query = "
-                        match
-                        $u isa user, has email $e;
-                        get
-                        $e;
-                        ";
-        let response = tx.query().get(get_query)?;
-        for (i, cm) in response.enumerate() {
-            let email_concept = cm.unwrap().get("e").unwrap().clone();
-            let email = match email_concept {
-                Concept::Attribute(Attribute {
-                    value: Value::String(value),
-                    ..
-                }) => value,
-                _ => unreachable!(),
-            };
-            println!("Email #{}: {}", (i + 1).to_string(), email)
+        {
+            let session = Session::new(db, SessionType::Data)?;
+            {
+                let transaction = session.transaction(TransactionType::Read)?;
+                let get_query = "
+                                match
+                                $u isa user, has email $e;
+                                get
+                                $e;
+                                ";
+                let response = transaction.query().get(get_query)?;
+                for (i, cm) in response.enumerate() {
+                    let email_concept = cm.unwrap().get("e").unwrap().clone();
+                    let email = match email_concept {
+                        Concept::Attribute(Attribute {
+                            value: Value::String(value),
+                            ..
+                        }) => value,
+                        _ => unreachable!(),
+                    };
+                    println!("Email #{}: {}", (i + 1).to_string(), email)
+                }
+            }
         }
         // end::get[]
     }
 
     {   // tag::infer-rule[]
         let db = databases.get(DB_NAME)?;
-        let session = Session::new(db, SessionType::Schema)?;
-        let tx = session.transaction(TransactionType::Write)?;
-        let define_query = "
-                            define
-                            rule users:
-                            when {
-                                $u isa user;
-                            } then {
-                                $u has name 'User';
-                            };
-                            ";
-        tx.query().define(define_query).resolve()?;
-        tx.commit().resolve()?;
+       {
+            let session = Session::new(db, SessionType::Schema)?;
+            {
+                let transaction = session.transaction(TransactionType::Write)?;
+                let define_query = "
+                                    define
+                                    rule users:
+                                    when {
+                                        $u isa user;
+                                    } then {
+                                        $u has name 'User';
+                                    };
+                                    ";
+                transaction.query().define(define_query).resolve()?;
+                transaction.commit().resolve()?;
+            }
+        }
         // end::infer-rule[]
         // tag::infer-fetch[]
         let db = databases.get(DB_NAME)?;
         let options = Options::new().infer(true);
-        let session = Session::new(db, SessionType::Data)?;
-        let tx = session.transaction_with_options(TransactionType::Read, options)?;
-        let fetch_query = "
-                            match
-                            $u isa user;
-                            fetch
-                            $u: name, email;
-                            ";
-        let response = tx.query().fetch(fetch_query)?;
-        for (i, json) in response.enumerate() {
-            println!("User #{}: {}", (i + 1).to_string(), json.unwrap().to_string())
+        {
+            let session = Session::new(db, SessionType::Data)?;
+            {
+                let transaction = session.transaction_with_options(TransactionType::Read, options)?;
+                let fetch_query = "
+                                    match
+                                    $u isa user;
+                                    fetch
+                                    $u: name, email;
+                                    ";
+                let response = transaction.query().fetch(fetch_query)?;
+                for (i, json) in response.enumerate() {
+                    println!("User #{}: {}", (i + 1).to_string(), json.unwrap().to_string())
+                }
+            }
         }
         // end::infer-fetch[]
     }
