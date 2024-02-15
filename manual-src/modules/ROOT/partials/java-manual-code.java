011b171cfbf9001e7cc56c846cac6dcc4b64513d
Vladimir Izmalkov <vladimir@vaticle.com>
2024-02-15 21:29:06 +0000

    Update the data page for Java

--- manual-src/modules/ROOT/partials/java-manual-code.java
+++ manual-src/modules/ROOT/partials/java-manual-code.java
@@ -6,12 +6,16 @@ import com.vaticle.typedb.driver.api.TypeDBOptions;
 import com.vaticle.typedb.driver.api.TypeDBSession;
 import com.vaticle.typedb.driver.api.TypeDBTransaction;
 import com.vaticle.typedb.driver.api.concept.Concept;
+import com.vaticle.typedb.driver.api.concept.thing.Entity;
 import com.vaticle.typedb.driver.api.concept.type.AttributeType;
 import com.vaticle.typedb.driver.api.concept.type.EntityType;
+import com.vaticle.typedb.driver.api.concept.type.ThingType;
 import com.vaticle.typedb.driver.api.concept.value.Value;
 import com.vaticle.typedb.driver.api.logic.Rule;
 import com.vaticle.typeql.lang.TypeQL;
 import com.vaticle.typeql.lang.pattern.Pattern;
+import java.util.Collections;
+import java.util.Set;
 // end::import[]
 public class Main {
     public static void main(String[] args) {
@@ -223,19 +227,32 @@ public class Main {
             try (TypeDBTransaction Transaction = session.transaction(TypeDBTransaction.Type.WRITE)) {
                 Rule oldRule = Transaction.logic().getRule("users").resolve();
                 System.out.println("Rule label: " + oldRule.getLabel());
+                System.out.println("  Condition: " + oldRule.getWhen().toString());
+                System.out.println("  Conclusion: " + oldRule.getThen().toString());
                 Pattern condition = TypeQL.parsePattern("{$u isa user, has email $e; $e contains '@vaticle.com';}");
                 Pattern conclusion = TypeQL.parsePattern("$u has name 'Employee'");
                 Rule newRule = Transaction.logic().putRule("Employee", condition, conclusion).resolve();
-                Transaction.logic().getRules().forEach(result -> {
-                    System.out.println("Rule: " + result.getLabel());
-                    System.out.println("  Condition: " + result.getWhen().toString());
-                    System.out.println("  Conclusion: " + result.getThen().toString());
-                });
+                Transaction.logic().getRules().forEach(result -> System.out.println(result.getLabel()));
                 newRule.delete(Transaction).resolve();
                 Transaction.commit();
             }
         }
         // end::rules-api[]
+        // tag::data-api[]
+        try (TypeDBSession session = driver.session(DB_NAME, TypeDBSession.Type.DATA)) {
+            try (TypeDBTransaction Transaction = session.transaction(TypeDBTransaction.Type.WRITE)) {
+                Set<ThingType.Annotation> annotations = Collections.emptySet();
+                Transaction.concepts().getEntityType("user").resolve().getInstances(Transaction).forEach(user -> {
+                    System.out.println("User");
+                    user.getHas(Transaction, annotations).forEach(attribute ->
+                        System.out.println(attribute.getType().getLabel().toString()+ ": " + attribute.getValue().toString()));
+                });
+                Entity new_user = Transaction.concepts().getEntityType("user").resolve().create(Transaction).resolve();
+                new_user.delete(Transaction).resolve();
+                Transaction.commit();
+            }
+        }
+        // end::data-api[]
         driver.close();
     }
 }
