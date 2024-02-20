126173cb813b6e9db28d2538449d1c8d6541f4b5
Krishnan Govindraj <krishnan@vaticle.com>
2024-02-20 20:30:07 +0000

    Quieter by default

--- learn-src/modules/ROOT/attachments/dockerise/bootstrapper.py
+++ learn-src/modules/ROOT/attachments/dockerise/bootstrapper.py
@@ -4,10 +4,13 @@ import subprocess
 import urllib
 import yaml
 
+from os import environ
 from time import sleep
 
 BOOTSTRAPPER_VERSION = "1.0.0"
-DEFAULT_CONFIG_YML = "https://raw.githubusercontent.com/vaticle/typedb-docs/master/learn-src/modules/ROOT/attachments/config.yml"
+DEFAULT_CONFIG_YML = "https://raw.githubusercontent.com/krishnangovindraj/typedb-docs/dockerised-samples/learn-src/modules/ROOT/attachments/config.yml"
+PRINT_DEBUG = environ.get("DEBUG_BOOTSTRAPPER", "false").lower() != "false"
+BOOTSTRAPPER_PORT = 1730
 
 class TypeDBBootstrapperException(Exception):
     pass
@@ -31,7 +34,9 @@ def download_to_file(url, filepath):
         f.write(content)
 
 
-def _run_cmd(cmd, silent=False):
+def _run_cmd(cmd, silent=None):
+    if silent is None:
+        silent = not PRINT_DEBUG
     output_to = subprocess.DEVNULL if silent else None
     result = subprocess.run(cmd, stdout=output_to, stderr=output_to)
     if result.returncode != 0:
@@ -39,51 +44,49 @@ def _run_cmd(cmd, silent=False):
 
 
 def install_typedb(version):
+    print("Installing TypeDB: " + version)
     _run_cmd(["apt", "update", "-y"])
     _run_cmd(["apt", "install", "-y", "default-jre", "typedb=%s"%version])
     version_output = subprocess.check_output(["typedb", "server", "--version"])
     version_line = version_output.decode().strip().split("\n")[-1]
     installed_version= version_line[len("Version:"):].strip()
     assert installed_version == version
-    print("Successfully installed typedb " + installed_version)
+    print("Successfully installed TypeDB: " + installed_version)
 
 def start_typedb():
-    process = subprocess.Popen(["typedb","server"])
+    print("Starting TypeDB for bootstrap")
+    output_to = None if PRINT_DEBUG else subprocess.DEVNULL
+    process = subprocess.Popen(["typedb","server", "--server.address=127.0.0.1:%d"%BOOTSTRAPPER_PORT], stdout=output_to, stderr=output_to )
     for i in range(10):
         try:
-            _run_cmd(["typedb", "console", "--command=database list"], i!=9)
+            console_command(["--command=database list"], i!=9)
             return process
         except TypeDBBootstrapperException as e:
             sleep(2)
     raise TypeDBBootstrapperException("Could not start typedb server")
 
+def console_command(cmd, silent=True):
+    return _run_cmd(["typedb", "console", "--core=127.0.0.1:%d"%BOOTSTRAPPER_PORT] + cmd, silent)
 
 def install_datasets(dataset_root, datasets):
     for i, dataset in enumerate(datasets, start=1):
         print("%d/%d Loading %s"%(i, len(datasets), dataset))
         schema_file = "%s.schema.tql"%dataset
         data_file = "%s.data.tql"%dataset
-        print(schema_file, data_file)
         download_to_file(urllib.parse.urljoin(dataset_root, datasets[dataset]["schema"]), schema_file)
         download_to_file(urllib.parse.urljoin(dataset_root, datasets[dataset]["data"]), data_file)
-        _run_cmd(["typedb", "console", "--command=database create %s"%dataset])
-        _run_cmd(["typedb", "console", "--command=transaction %s schema write"%dataset, "--command=source %s"%schema_file, "--command=commit"])
-        _run_cmd(["typedb", "console", "--command=transaction %s data write"%dataset, "--command=source %s"%data_file, "--command=commit"])
+        console_command(["--command=database create %s"%dataset])
+        console_command(["--command=transaction %s schema write"%dataset, "--command=source %s"%schema_file, "--command=commit"])
+        console_command(["--command=transaction %s data write"%dataset, "--command=source %s"%data_file, "--command=commit"])
         print("%d/%d Completed loading dataset: %s"%(i, len(datasets), dataset))
 def main():
     try:
         args = parse_args()
         raw_yaml = http_get(DEFAULT_CONFIG_YML) if args.config_yml is None else open(args.config_yml, 'r').read()
         config = yaml.safe_load(raw_yaml)
-        if args.dataset_root is not None: config['dataset-root'] = args.dataset_root
-
-        print(config)
-
-        config["dataset-root"] = "https://raw.githubusercontent.com/krishnangovindraj/typedb-docs/dockerised-samples/learn-src/modules/ROOT/attachments/" # TODO: REMOVE. Testing only
-
         if config['bootstrapper-version'] != BOOTSTRAPPER_VERSION:
             raise TypeDBBootstrapperException("This bootstrapper is outdated and will not run. Please update to version: " + config['boostrapper-version'])
-
+        if args.dataset_root is not None: config['dataset-root'] = args.dataset_root
         install_typedb(config['typedb-version'])
         with start_typedb() as typedb_process:
             try:
