c551eb035e4b0ddb3cdee2ff48e53ac410d8a05b
Krishnan Govindraj <krishnan@vaticle.com>
2024-02-21 11:33:01 +0000

    Update bootstrapper.py

--- learn-src/modules/ROOT/attachments/dockerise/bootstrapper.py
+++ learn-src/modules/ROOT/attachments/dockerise/bootstrapper.py
@@ -1,47 +1,53 @@
-import argparse
+import os
 import requests
 import subprocess
 import urllib
 import yaml
 
-from os import environ
 from time import sleep
 
-BOOTSTRAPPER_VERSION = "1.0.0"
-DEFAULT_CONFIG_YML = "https://raw.githubusercontent.com/vaticle/typedb-docs/master/learn-src/modules/ROOT/attachments/config.yml"
-PRINT_DEBUG = environ.get("DEBUG_BOOTSTRAPPER", "false").lower() != "false"
-BOOTSTRAPPER_PORT = 1730
+BOOTSTRAPPER_VERSION = "1.0.0"  # Will fail if the config.yml is of a different version.
+# DEFAULT_CONFIG_YML = "https://raw.githubusercontent.com/vaticle/typedb-docs/master/learn-src/modules/ROOT/attachments/config.yml"
+DEFAULT_CONFIG_YML = "https://raw.githubusercontent.com/krishnangovindraj/typedb-docs/dockerised-samples/learn-src/modules/ROOT/attachments/config.yml"
+BOOTSTRAPPER_PORT = 1730 # during dataset loading, typedb runs on a different port to to remain unreachable.
+
+VERBOSE = os.environ.get("BOOTSTRAPPER_VERBOSE", "false").lower() != "false"
+# Overrides for testing with local files
+CONFIG_OVERRIDE = os.environ.get("BOOTSTRAPPER_CONFIG", None)
+DATASET_ROOT_OVERRIDE = os.environ.get("BOOTSTRAPPER_DATASET_ROOT", None)
 
 class TypeDBBootstrapperException(Exception):
     pass
 
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--config_yml', required=False, help="Override the config.yml used")
-    parser.add_argument('--dataset_root', required=False, help="Override the dataset root")
-    return parser.parse_args()
 
-def http_get(url):
+def _is_url(s):
+    return s.startswith("https://") or s.startswith("http://")
+
+def _http_get(url):
     resp = requests.get(url)
     if resp.status_code == 200:
         return resp.text
     else:
         raise TypeDBBootstrapperException("Could not download sample.yml from: " + url)
 
-def download_to_file(url, filepath):
-    content = http_get(url)
-    with open(filepath, "w") as f:
-        f.write(content)
+def _console_command(cmd, silence_errors=False):
+    return _run_cmd(["typedb", "console", "--core=127.0.0.1:%d"%BOOTSTRAPPER_PORT] + cmd, silence_errors)
 
-
-def _run_cmd(cmd, silent=None):
-    if silent is None:
-        silent = not PRINT_DEBUG
-    output_to = subprocess.DEVNULL if silent else None
-    result = subprocess.run(cmd, stdout=output_to, stderr=output_to)
+def _run_cmd(cmd, silence_errors=False):
+    stdout_to = None if VERBOSE else subprocess.DEVNULL
+    stderr_to = subprocess.DEVNULL if silence_errors and not VERBOSE else None
+    result = subprocess.run(cmd, stdout=stdout_to, stderr=stderr_to)
     if result.returncode != 0:
         raise TypeDBBootstrapperException("Running command failed: " + " ".join(cmd))
 
+def load_config(config_path):
+    if CONFIG_OVERRIDE is not None:
+        print("BOOTSTRAPPER_CONFIG was defined. Loading config from %s"%CONFIG_OVERRIDE)
+        raw_yaml = _http_get(config_path) if _is_url(config_path) else open(CONFIG_OVERRIDE, 'r').read()
+    else:
+        print("Loading config from %s"%config_path)
+        raw_yaml = _http_get(config_path)
+    return yaml.safe_load(raw_yaml)
 
 def install_typedb(version):
     print("Installing TypeDB: " + version)
@@ -55,51 +61,59 @@ def install_typedb(version):
 
 def start_typedb():
     print("Starting TypeDB for bootstrap")
-    output_to = None if PRINT_DEBUG else subprocess.DEVNULL
-    process = subprocess.Popen(["typedb","server", "--server.address=127.0.0.1:%d"%BOOTSTRAPPER_PORT], stdout=output_to, stderr=output_to )
+    output_to = None if VERBOSE else subprocess.DEVNULL
+    process = subprocess.Popen(["typedb","server", "--server.address=127.0.0.1:%d"%BOOTSTRAPPER_PORT], stdout=output_to, stderr=output_to)
     for i in range(10):
         try:
-            console_command(["--command=database list"], i!=9)
+            _console_command(["--command=database list"], i != 9)
             return process
         except TypeDBBootstrapperException as e:
             sleep(2)
     raise TypeDBBootstrapperException("Could not start typedb server")
 
-def console_command(cmd, silent=True):
-    return _run_cmd(["typedb", "console", "--core=127.0.0.1:%d"%BOOTSTRAPPER_PORT] + cmd, silent)
+def _download_dataset(from_root, from_relative, to_path):
+    if DATASET_ROOT_OVERRIDE is not None:
+        content = _http_get(urllib.parse.urljoin(DATASET_ROOT_OVERRIDE, from_relative)) if _is_url(DATASET_ROOT_OVERRIDE) else open(os.path.join(DATASET_ROOT_OVERRIDE, from_relative), 'r').read()
+    else:
+        content = _http_get(urllib.parse.urljoin(from_root, from_relative))
+    with open(to_path, "w") as f:
+        f.write(content)
 
 def install_datasets(dataset_root, datasets):
+    if DATASET_ROOT_OVERRIDE is not None:
+        print("BOOTSTRAPPER_DATASET_ROOT detected. Datasets will be loaded from: ", DATASET_ROOT_OVERRIDE)
+    else:
+        print("Loading datasets from: ", dataset_root)
+
     for i, dataset in enumerate(datasets, start=1):
         print("%d/%d Loading %s"%(i, len(datasets), dataset))
         schema_file = "%s.schema.tql"%dataset
         data_file = "%s.data.tql"%dataset
-        download_to_file(urllib.parse.urljoin(dataset_root, datasets[dataset]["schema"]), schema_file)
-        download_to_file(urllib.parse.urljoin(dataset_root, datasets[dataset]["data"]), data_file)
-        console_command(["--command=database create %s"%dataset])
-        console_command(["--command=transaction %s schema write"%dataset, "--command=source %s"%schema_file, "--command=commit"])
-        console_command(["--command=transaction %s data write"%dataset, "--command=source %s"%data_file, "--command=commit"])
+        _download_dataset(dataset_root, datasets[dataset]["schema"], schema_file)
+        _download_dataset(dataset_root, datasets[dataset]["data"], data_file)
+        _console_command(["--command=database create %s" % dataset])
+        _console_command(["--command=transaction %s schema write" % dataset, "--command=source %s" % schema_file, "--command=commit"])
+        _console_command(["--command=transaction %s data write" % dataset, "--command=source %s" % data_file, "--command=commit"])
         print("%d/%d Completed loading dataset: %s"%(i, len(datasets), dataset))
+
 def main():
     try:
-        args = parse_args()
-        raw_yaml = http_get(DEFAULT_CONFIG_YML) if args.config_yml is None else open(args.config_yml, 'r').read()
-        config = yaml.safe_load(raw_yaml)
+        config = load_config(DEFAULT_CONFIG_YML)
         if config['bootstrapper-version'] != BOOTSTRAPPER_VERSION:
             raise TypeDBBootstrapperException("This bootstrapper is outdated and will not run. Please update to version: " + config['boostrapper-version'])
-        if args.dataset_root is not None: config['dataset-root'] = args.dataset_root
-
-        install_typedb(config['typedb-version'])
+        # install_typedb(config['typedb-version'])
         with start_typedb() as typedb_process:
             try:
                 install_datasets(config['dataset-root'], config['datasets'])
             finally:
+                print("Shutting down TypeDB.")
                 typedb_process.terminate()
                 typedb_process.wait()
         print("Bootstrapping complete!")
     except Exception as e:
-        print("Error during bootstrapping. Run with environment variable DEBUG_BOOTSTRAPPER=True for subcommand output")
-
-        if PRINT_DEBUG: raise e
+        print("Error during bootstrapping. Run with environment variable BOOTSTRAPPER_VERBOSE=True for subcommand output")
+        if VERBOSE:
+            raise e
         else:
             print(str(e))
             quit(1)
