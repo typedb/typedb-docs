d2772487bd1ed6b59e881d20d2a9b858daa6b33e
James Whiteside <james.whiteside@vaticle.com>
2024-03-21 18:39:23 +0000

    Updated Lesson 11.1.

--- learn-src/modules/ROOT/attachments/python-example-code-10.py
+++ learn-src/modules/ROOT/attachments/python-example-code-10.py
@@ -1,17 +1,6 @@
 import datetime
-
-# 10 manipulating stateful objects
-
-from typedb.driver import TypeDB, SessionType, TransactionType
+from typedb.api.concept.type.role_type import RoleType
 from typedb.api.connection.credential import TypeDBCredential
-
-from typing import Iterator
-from typedb.api.answer.concept_map import ConceptMap
-from typedb.api.concept.thing.entity import Entity
-from typedb.api.concept.thing.relation import Relation
-from typedb.api.connection.transaction import TypeDBTransaction
-
-# 10.1 retrieving objects
 
 ADDRESS = "localhost:1730"
 USERNAME = "username"
@@ -20,13 +9,32 @@ DATABASE = "bookstore"
 password = input("Enter password: ")
 credential = TypeDBCredential(USERNAME, password, tls_enabled=True)
 
+# 11 manipulating stateful objects
+
+from typedb.driver import TypeDB, SessionType, TransactionType
+
+from typing import Iterator
+from typedb.api.connection.transaction import TypeDBTransaction
+from typedb.api.answer.concept_map import ConceptMap
+from typedb.api.concept.thing.entity import Entity
+from typedb.api.concept.thing.relation import Relation
+from typedb.api.concept.thing.attribute import Attribute
+from typedb.api.concept.type.entity_type import EntityType
+from typedb.api.concept.type.relation_type import RelationType
+from typedb.api.concept.type.attribute_type import AttributeType
+from typedb.api.concept.thing.thing import Thing
+
+# 11.1 retrieving objects
+
 with TypeDB.cloud_driver(ADDRESS, credential) as driver:
     with driver.session(DATABASE, SessionType.DATA) as session:
         with session.transaction(TransactionType.READ) as transaction:
             results: Iterator[ConceptMap] = transaction.query.get("""
                 match
                 $user isa user;
-                $execution ($user, $action) isa action-execution;
+                $execution ($user, $action) isa action-execution,
+                    has timestamp $timestamp;
+                $action isa $action-type;
                 get;
             """)
 
@@ -34,6 +42,8 @@ with TypeDB.cloud_driver(ADDRESS, credential) as driver:
                 user: Entity = result.get("user").as_entity()
                 action: Entity = result.get("action").as_entity()
                 execution: Relation = result.get("execution").as_relation()
+                timestamp: Attribute = result.get("timestamp").as_attribute()
+                action_type: EntityType = result.get("action-type").as_entity_type()
 
 
 def get_books_in_genre(transaction: TypeDBTransaction, genre: str) -> Iterator[Entity]:
@@ -81,3 +91,77 @@ def get_discounted_books(transaction: TypeDBTransaction) -> Iterator[Entity]:
         book = result.get("book").as_entity()
 
         yield book
+
+
+with TypeDB.cloud_driver(ADDRESS, credential) as driver:
+    with driver.session(DATABASE, SessionType.DATA) as session:
+        with session.transaction(TransactionType.READ) as transaction:
+            book_type: EntityType = transaction.concepts.get_entity_type("book").resolve()
+            contribution_type: RelationType = transaction.concepts.get_relation_type("contribution").resolve()
+            page_count_type: AttributeType = transaction.concepts.get_attribute_type("page-count").resolve()
+
+
+with TypeDB.cloud_driver(ADDRESS, credential) as driver:
+    with driver.session(DATABASE, SessionType.DATA) as session:
+        with session.transaction(TransactionType.READ) as transaction:
+            book_type: EntityType
+            contribution_type: RelationType
+            page_count_type: AttributeType
+
+            books: Iterator[Entity] = book_type.get_instances(transaction)
+            contributions: Iterator[Relation] = contribution_type.get_instances(transaction)
+            page_counts: Iterator[Attribute] = page_count_type.get_instances(transaction)
+
+
+def get_users(transaction: TypeDBTransaction) -> Iterator[Entity]:
+    results = transaction.query.get("""
+        match
+        $user isa user;
+        get;
+    """)
+
+    for result in results:
+        user = result.get("user").as_entity()
+
+        yield user
+
+
+def get_users(transaction: TypeDBTransaction) -> Iterator[Entity]:
+    user_type = transaction.concepts.get_entity_type("user").resolve()
+    users = user_type.get_instances(transaction)
+    return users
+
+
+with TypeDB.cloud_driver(ADDRESS, credential) as driver:
+    with driver.session(DATABASE, SessionType.DATA) as session:
+        with session.transaction(TransactionType.READ) as transaction:
+            book: Entity
+            attributes: Iterator[Attribute] = book.get_has(transaction)
+            relations: Iterator[Relation] = book.get_relations(transaction)
+
+
+def cast_thing(thing: Thing) -> Entity | Relation:
+    if thing.is_entity():
+        return thing.as_entity()
+    if thing.is_relation():
+        return thing.as_relation()
+
+
+with TypeDB.cloud_driver(ADDRESS, credential) as driver:
+    with driver.session(DATABASE, SessionType.DATA) as session:
+        with session.transaction(TransactionType.READ) as transaction:
+            book: Entity
+            work_role: RoleType
+            contributions = book.get_relations(transaction, work_role)
+
+
+def get_related_entities(transaction: TypeDBTransaction, entity: Entity) -> Iterator[Entity]:
+    for relation in entity.get_relations(transaction):
+        for player in relation.get_players_by_role_type(transaction):
+            if not player.is_entity():
+                continue
+            elif player == entity:
+                continue
+            else:
+                yield player.as_entity()
+
