ae1a2dcb68d7043b4fa33c21f1a30250bb8ba141
cxdorn <christoph@typedb.com>
2025-02-14 12:07:46 +0000

    directory walking

--- code_test/main.py
+++ code_test/main.py
@@ -1,7 +1,26 @@
 import importlib
+import logging
 import sys
+import os
 from typing import Dict, List
 from parser.parser import parse_programs
+
+# Logging config
+logger = logging.getLogger('main')
+logging.basicConfig(level=logging.INFO,
+                    format="%(asctime)s [%(filename)s:%(lineno)03d]: %(message)s",
+                    datefmt="%Y-%m-%d %H:%M:%S")
+
+
+# Directories to code test
+DIRECTORIES = [
+    os.path.join(os.path.dirname(__file__), "tests"),
+    os.path.join(os.path.dirname(__file__), "../home/modules/ROOT/pages"),
+    os.path.join(os.path.dirname(__file__), "../manual/modules/ROOT/pages"),
+    os.path.join(os.path.dirname(__file__), "../typeql/modules/ROOT/pages"),
+    os.path.join(os.path.dirname(__file__), "../drivers/modules/ROOT/pages"),
+    os.path.join(os.path.dirname(__file__), "../academy/modules/ROOT/pages")
+]
 
 
 def parse_adoc_config(adoc_path: str, key_names: List[str]) -> Dict[str, str]:
@@ -22,23 +41,42 @@ def parse_adoc_config(adoc_path: str, key_names: List[str]) -> Dict[str, str]:
 
     return adoc_config
 
-
-def test_one_file(runner, lang: str, adoc_path: str) -> None:
-    # Parse
-    adoc_config = parse_adoc_config(adoc_path, runner.ADOC_CONFIG_KEYS)
-
-    # Test
+def test_programs_in_file(runner, lang: str, adoc_path: str, adoc_config):
     try:
         parsed_programs = parse_programs(adoc_path, lang)
         runner.test_programs(parsed_programs, adoc_path, adoc_config)
+        logger.info(f"RESULTS: {runner.success_count} SUCCESSFUL, {runner.failure_count} FAILED")
+        return runner.failure_count > 0
     except Exception as e:
         print(f"ERROR: {e}")
         sys.exit(1)
 
 
-def test_all_files(runner, lang: str):
-    return None
+def test_one_file(runner, lang: str, adoc_path: str):
+    adoc_config = parse_adoc_config(adoc_path, runner.adoc_keys)
+    if runner.check_config(adoc_config):
+        test_programs_in_file(runner, lang, adoc_path, adoc_config)
+    else:
+        logger.info(f"[{adoc_path}]: Bad adoc attributes in provided file.")
 
+
+def test_all_files(runner, lang: str):
+    files_with_failures = []
+    for directory in DIRECTORIES:
+        for root, _, files in os.walk(directory):
+            for file in files:
+                if file.endswith(".adoc"):
+                    adoc_path = os.path.relpath(os.path.join(root, file))
+                    adoc_config = parse_adoc_config(adoc_path, runner.adoc_keys)
+                    if adoc_config[runner.adoc_keys[0]] is not None:
+                        if runner.check_config(adoc_config):
+                            logger.info(f"TESTING Bad adoc attributes in provided file.")
+                            if not test_programs_in_file(runner, lang, adoc_path, adoc_config):
+                                files_with_failures.append(adoc_path)
+                        else:
+                            logger.info(f"Bad adoc attributes in file.")
+
+    logger.info(f"SUMMARY: {len(files_with_failures)} file(s) had test failures:\n>> " + "\n>> ".join(files_with_failures))
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
@@ -58,8 +96,10 @@ if __name__ == "__main__":
         print(f"Runner class for language '{lang}' not found. msg: {e}")
         sys.exit(1)
 
-    if sys.argv[2]:
+    if len(sys.argv) > 2:
+        logger.info(f"Starting testing in single file mode")
         file_path = sys.argv[2]
         test_one_file(runner, lang, file_path)
     else:
+        logger.info(f"Starting testing in multi-file mode")
         test_all_files(runner, lang)
