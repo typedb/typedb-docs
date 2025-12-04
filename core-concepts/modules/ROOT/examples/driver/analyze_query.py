745460cbf964bdaf4f2fe71cb5db763ae2365d82
Krishnan Govindraj <krishnan@typedb.com>
2025-12-04 13:15:29 +0100

    More asserting tests

--- core-concepts/modules/ROOT/examples/driver/analyze_query.py
+++ core-concepts/modules/ROOT/examples/driver/analyze_query.py
@@ -8,4 +8,24 @@ with driver.transaction(DB_NAME, TransactionType.READ) as tx:
 
     # 2. Resolve the promise if you want to access the result or receive an error as an exception
     analyzed = promise.resolve()
+
 #end::analyze[]
+
+#tag::get_conjunction[]
+pipeline = analyzed.pipeline()
+stages = list(pipeline.stages())
+block_id = stages[0].as_match().block()
+root_conjunction = pipeline.conjunction(block_id)
+
+#end::get_conjunction[]
+
+#tag::get_first_branch_isa[]
+constraints = list(root_conjunction.constraints())
+or_constraint = constraints[0]
+assert or_constraint.is_or()
+branches = [pipeline.conjunction(id) for id in or_constraint.as_or().branches()]
+first_branch_constraints = list(branches[0].constraints())
+first_branch_isa = first_branch_constraints[0]
+assert first_branch_isa.is_isa()
+
+#end::get_first_branch_isa[]
