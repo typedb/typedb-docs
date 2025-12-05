9895f03d7fff43a9a69102849aa8cbe2fb94bc8e
Dmitrii Ubskii <18616863+dmitrii-ubskii@users.noreply.github.com>
2025-12-05 14:07:41 +0000

    add word boundary to split

--- test/code/runners/typeql_runner.py
+++ test/code/runners/typeql_runner.py
@@ -94,7 +94,7 @@ class TypeqlRunner(BaseRunner):
         return FailureMode.NoFailure
 
     def run_transaction(self, queries: List[str], type: TransactionType, rollback=False) -> Union[int, None]:
-        queries = [q for qs in queries for q in re.split(r'end\s*;', qs) if q.strip()]
+        queries = [q for qs in queries for q in re.split(r'\bend\s*;', qs) if q.strip()]
         with self.driver.transaction(self.db, type) as tx:
             try:
                 for q in queries:
